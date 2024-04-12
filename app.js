d42ae0e07c41ad4c1eb4fb5623ef6c7ccabe5387
Artur dos Santos Shon <sky_artur@hotmail.com>
2024-04-11 23:45:58 -0300

    Criando a base do projeto da api.

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-
+app.set('views', path.join(__dirname, ''))
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, 'public')))
+
 
 
 const routerIndex = require('./routes/index')
