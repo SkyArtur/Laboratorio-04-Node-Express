4be69040a89e1efc72a3cbdbf14cf11200b60bba
Artur dos Santos Shon <sky_artur@hotmail.com>
2024-04-11 23:46:29 -0300

    Criando a base do projeto da api.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.set('views', path.join(__dirname, ''))
+
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: true }))
