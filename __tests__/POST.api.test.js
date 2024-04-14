b0e3090708855112c5d3ccfa42f39c263971f0f6
Artur dos Santos Shon <sky_artur@hotmail.com>
2024-04-14 12:20:46 -0300

    Implementando instalação docker.

--- __tests__/POST.api.test.js
+++ __tests__/POST.api.test.js
@@ -3,18 +3,19 @@ const options = {
     headers: {
         'content-type': 'application/json',
     },
-    body: JSON.stringify({
+    body: {}
+}
+
+
+
+test('Testando o verbo POST na URI /estoques', async () => {
+    options.body = JSON.stringify({
         produto: 'Pitangas',
         quantidade: 1500,
         custo: 955.65,
         lucro: 45,
         data: '2024-04-11'
     })
-}
-
-
-
-test('Testando o verbo POST na URI /estoques', async () => {
     const data = await fetch('http://localhost:3000/estoque/', options)
         .then(r => {
             if (!r.ok) return r.status
@@ -22,4 +23,18 @@ test('Testando o verbo POST na URI /estoques', async () => {
         })
         .catch(err => err)
     expect(data.forEach(e => e["registrar_produto_no_estoque"])).toBeFalsy()
+})
+
+test('Testando o verbo POST na URI /vendas', async () => {
+    options.body = JSON.stringify({
+        produto: 'Pitangas',
+        quantidade: 10,
+    })
+    const data = await fetch('http://localhost:3000/vendas/', options)
+        .then(r => {
+            if (!r.ok) return r.status
+            return r.json()
+        })
+        .catch(err => err)
+    expect(data[0]['registrar_venda']).toBeTruthy()
 })
